dd26a66ba429ae0661cf3f5ed4b7e917858b4fb6
Iain Matchett <matchett808@users.noreply.github.com>
2018-11-29 22:21:40 +0000

    Added bootstrap.php
    Added bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,41 @@
+<?php
+  
+$this->bind("/api/grp/:group", function($params) {  
+    
+    
+    $group = $params["group"];
+    $allS = [];
+    $allC = [];
+    $allC = $this->module("collections")->collections();
+    $allS = $this->module("singletons")->singletons();
+    $options = [];
+
+    if ($lang = $this->param("lang", false)) $options["lang"] = $lang;
+    $options["populate"] = true;
+    if ($ignoreDefaultFallback = $this->param("ignoreDefaultFallback", false)) $options["ignoreDefaultFallback"] = $ignoreDefaultFallback;
+    if ($user) $options["user"] = $user;
+
+    foreach($allS as $key => $value) { 
+        $singleton = $this->module("singletons")->getData($key, $options);
+        if($value["group"] == $group){ 
+            $singletons[] = $singleton; 
+        }
+    }
+    
+    foreach($allC as $key => $value) { 
+        //$collections = $this->module("collections")->getData($key, $options);
+        $collection = $this->module("collections")->find($key, $options);
+        if($value["group"] == $group){
+            $collections[] = $collection;
+        }
+    }
+    $returnArray = [];
+    $returnArray["singletons"] = $singletons;
+    $returnArray["collections"] = $collections;
+    echo json_encode($returnArray);
+    
+    
+    exit();
+
+
+});
